5e63bca18dd89ef0b749f0b2c0a4cf36544c97ff
Helfik <helfik123_1998@mail.ru>
2023-01-20 00:52:35 +0300

    Register all models

--- VI/VI_app/admin.py
+++ VI/VI_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(PerevalAdded)
+admin.site.register(ActivitiesType)
+admin.site.register(PerevalAreas)
+admin.site.register(AutumnLevel)
+admin.site.register(SummerLevel)
+admin.site.register(SpringLevel)
+admin.site.register(WinterLevel)
+admin.site.register(PerevalImages)
+admin.site.register(Coords)
